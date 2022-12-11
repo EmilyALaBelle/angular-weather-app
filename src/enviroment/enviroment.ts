1c571cbf6048bff21d1ec1e9117b2e7799af7dc5
Emily LaBelle <emilylabelle8@gmail.com>
2022-12-11 13:59:55 -0500

    created weather app

---
+++ src/enviroment/enviroment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather",
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'weather-by-api-ninjas.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'e320a0de0fmsh99f3f85e0ad0d06p165e2djsn89c89c21c376'
+};
